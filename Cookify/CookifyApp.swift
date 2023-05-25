6b479fd7e40b06898429044b812ac3786b189905
Ayman <65835687+ceobitch@users.noreply.github.com>
2023-05-25 11:27:10 -0500

    Initial Commit

---
+++ Cookify/CookifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookifyApp.swift
+//  Cookify
+//
+//  Created by ceo on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct CookifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
