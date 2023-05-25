a7529c245a2281bd0e1c46c29e6d3bfdeab2e7de
Ayman <65835687+ceobitch@users.noreply.github.com>
2023-05-25 11:57:04 -0500

    Fixed Error 🌹
    There was a error regarding the "Id" value in the meal response. I have successfully fixed that

--- Cookify/Networking/Models/Meal.swift
+++ Cookify/Networking/Models/Meal.swift
@@ -11,8 +11,8 @@ struct Meal: Codable, Identifiable {
     let idMeal: String?
     let strMeal: String?
     let strMealThumb: String?
-
-    var id: String {
+    
+    var id: String? {
         return idMeal
     }
 }
